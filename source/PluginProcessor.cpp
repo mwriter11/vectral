005ffa6ebf94dd25a960b8db2698f1b88cb325a8
MWriter11 <159955656+mwriter11@users.noreply.github.com>
2024-02-18 18:59:26 +0000

    Implement delayline

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -130,7 +130,9 @@ void PluginProcessor::changeProgramName (int index, const juce::String& newName)
 //==============================================================================
 void PluginProcessor::process (juce::dsp::ProcessContextReplacing<float> context)
 {
+    int delayTimeInSamples = *parameters.getRawParameterValue("time") * delayLineSampleRate;
 
+    delayLine.setDelay(delayTimeInSamples);
 }
 
 void PluginProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
@@ -149,6 +151,14 @@ void PluginProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
     spec.sampleRate = sampleRate;
     spec.maximumBlockSize = samplesPerBlock;
     spec.numChannels = getTotalNumOutputChannels();
+
+    int DELAY_RATE = 24000;
+
+    delayLine.reset();
+    delayLine.prepare(spec);
+    delayLine.setDelay(DELAY_RATE);
+
+    delayLineSampleRate = sampleRate;
 }
 
 void PluginProcessor::releaseResources()
@@ -188,18 +198,28 @@ void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
     juce::dsp::AudioBlock<float> block (buffer);
 
-    process(juce::dsp::ProcessContextReplacing<float> (buffer));
+    // process(juce::dsp::ProcessContextReplacing<float> (buffer));
+
+    // TODO: Move to process method:
+    int delayTimeInSamples = *parameters.getRawParameterValue("time") * delayLineSampleRate;
+
+    delayLine.setDelay(delayTimeInSamples);
+
+    for (int channel; channel < totalNumInputChannels; ++channel)
+    {
+        auto* inSamples = buffer.getReadPointer(channel);
+        auto* outSamples = buffer.getWritePointer(channel);
+
+        for (int sampleIndex; sampleIndex < buffer.getNumSamples(); sampleIndex++)
+        {
+            // Left off at 14:44 of Spanish delay line example video
+        }
+    }
 }
 
 //==============================================================================
--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -46,10 +46,16 @@ private:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
     void parameterChanged(const juce::String& parameterId, float newValue) override;
 
-    float mix = 0.0;
-    float time = 0.0;
-    float regen = 0.0;
-    bool mod = false;
+    float mix = 0.0; // Decibels of gain
+    float time = 0.0; // TODO: Delay time in seconds?
+    float regen = 0.0; // Also known as feedback
+    bool mod = false; // Turns on LFO if true
+
+    // TODO: Need processor duplicator?
+    // TODO: What does this arg do?
+    int DELAY_LINE_ARG = 192000;
+    juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> delayLine(DELAY_LINE_ARG);
+    double delayLineSampleRate(0.0);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginProcessor)
 };
